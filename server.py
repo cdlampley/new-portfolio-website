8040ff58c6574aa0947e2d3888fe6d5169d2f179
Cherie Lampley <cdlampley@gmail.com>
2022-11-07 21:42:55 -0500

    adding style template for portfolio site

--- server.py
+++ server.py
@@ -10,3 +10,15 @@ def homepage():
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+@app.route('/works')
+def my_work():
+    return render_template('works.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/components')
+def components():
+    return render_template('components.html')
